2fb2abc72270b2f8194e8f13ceabe63a2ad216a2
ElSanto146 <elsanto269@gmail.com>
2025-03-01 14:17:15 -0300

    Actualización3

--- target/generated-sources/annotations/com/amaras/spa/mapper/ITurnMapperImpl.java
+++ target/generated-sources/annotations/com/amaras/spa/mapper/ITurnMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-01T13:50:52-0300",
+    date = "2025-03-01T14:15:36-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
 )
 @Component
