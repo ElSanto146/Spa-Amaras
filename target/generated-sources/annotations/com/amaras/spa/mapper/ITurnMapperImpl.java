ac731d9fddba087b8878586e021cf04d03954c7c
ElSanto146 <elsanto269@gmail.com>
2025-03-01 10:34:48 -0300

    Primer commit para Render
    Se configuró para que la bd apunte a clever cloud y se agregó el dockerFile

---
+++ target/generated-sources/annotations/com/amaras/spa/mapper/ITurnMapperImpl.java
@@ -0,0 +1,148 @@
+package com.amaras.spa.mapper;
+
+import com.amaras.spa.model.dto.TurnDto;
+import com.amaras.spa.model.dto.TurnResponseDto;
+import com.amaras.spa.model.dto.UserDto;
+import com.amaras.spa.model.dto.UserResponseDto;
+import com.amaras.spa.model.entity.Turn;
+import com.amaras.spa.model.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-01T10:21:53-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Amazon.com Inc.)"
+)
+@Component
+public class ITurnMapperImpl implements ITurnMapper {
+
+    @Override
+    public Turn toTurn(TurnDto turnDto) {
+        if ( turnDto == null ) {
+            return null;
+        }
+
+        Turn.TurnBuilder turn = Turn.builder();
+
+        turn.user( userResponseDtoToUser( turnDto.getUser() ) );
+        turn.id( turnDto.getId() );
+        turn.date( turnDto.getDate() );
+        turn.hour( turnDto.getHour() );
+        turn.status( turnDto.getStatus() );
+
+        return turn.build();
+    }
+
+    @Override
+    public TurnDto toTurnDto(Turn turn) {
+        if ( turn == null ) {
+            return null;
+        }
+
+        TurnDto.TurnDtoBuilder turnDto = TurnDto.builder();
+
+        turnDto.id( turn.getId() );
+        turnDto.date( turn.getDate() );
+        turnDto.hour( turn.getHour() );
+        turnDto.status( turn.getStatus() );
+        turnDto.user( userToUserResponseDto( turn.getUser() ) );
+
+        return turnDto.build();
+    }
+
+    @Override
+    public List<TurnDto> toTurnDtoList(List<Turn> turns) {
+        if ( turns == null ) {
+            return null;
+        }
+
+        List<TurnDto> list = new ArrayList<TurnDto>( turns.size() );
+        for ( Turn turn : turns ) {
+            list.add( toTurnDto( turn ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public UserDto toUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto.UserDtoBuilder userDto = UserDto.builder();
+
+        userDto.id( user.getId() );
+        userDto.name( user.getName() );
+        userDto.username( user.getUsername() );
+        userDto.phone( user.getPhone() );
+        userDto.role( user.getRole() );
+        userDto.turns( turnListToTurnResponseDtoList( user.getTurns() ) );
+
+        return userDto.build();
+    }
+
+    @Override
+    public TurnResponseDto toTurnResponseDto(Turn turn) {
+        if ( turn == null ) {
+            return null;
+        }
+
+        TurnResponseDto.TurnResponseDtoBuilder turnResponseDto = TurnResponseDto.builder();
+
+        turnResponseDto.id( turn.getId() );
+        turnResponseDto.date( turn.getDate() );
+        turnResponseDto.hour( turn.getHour() );
+        turnResponseDto.status( turn.getStatus() );
+        turnResponseDto.user( userToUserResponseDto( turn.getUser() ) );
+
+        return turnResponseDto.build();
+    }
+
+    @Override
+    public UserResponseDto userToUserResponseDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponseDto.UserResponseDtoBuilder userResponseDto = UserResponseDto.builder();
+
+        userResponseDto.id( user.getId() );
+        userResponseDto.name( user.getName() );
+        userResponseDto.username( user.getUsername() );
+        userResponseDto.phone( user.getPhone() );
+
+        return userResponseDto.build();
+    }
+
+    protected User userResponseDtoToUser(UserResponseDto userResponseDto) {
+        if ( userResponseDto == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.id( userResponseDto.getId() );
+        user.name( userResponseDto.getName() );
+        user.username( userResponseDto.getUsername() );
+        user.phone( userResponseDto.getPhone() );
+
+        return user.build();
+    }
+
+    protected List<TurnResponseDto> turnListToTurnResponseDtoList(List<Turn> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<TurnResponseDto> list1 = new ArrayList<TurnResponseDto>( list.size() );
+        for ( Turn turn : list ) {
+            list1.add( toTurnResponseDto( turn ) );
+        }
+
+        return list1;
+    }
+}
---
+++ target/generated-sources/annotations/com/amaras/spa/mapper/IUserMapperImpl.java
@@ -0,0 +1,282 @@
+package com.amaras.spa.mapper;
+
+import com.amaras.spa.model.dto.TurnResponseDto;
+import com.amaras.spa.model.dto.UserDto;
+import com.amaras.spa.model.dto.UserDtoUpdate;
+import com.amaras.spa.model.dto.UserResponseDto;
+import com.amaras.spa.model.dto.UserUpResponseDto;
+import com.amaras.spa.model.entity.Turn;
+import com.amaras.spa.model.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-03-01T10:21:52-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Amazon.com Inc.)"
+)
+@Component
+public class IUserMapperImpl implements IUserMapper {
+
+    @Override
+    public User toUser(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.id( userDto.getId() );
+        user.name( userDto.getName() );
+        user.username( userDto.getUsername() );
+        user.phone( userDto.getPhone() );
+        user.role( userDto.getRole() );
+        user.turns( turnResponseDtoListToTurnList( userDto.getTurns() ) );
+
+        return user.build();
+    }
+
+    @Override
+    public User toUserUpdate(UserDtoUpdate userDtoUpdate) {
+        if ( userDtoUpdate == null ) {
+            return null;
+        }
+
+        User.UserBuilder user = User.builder();
+
+        user.id( userDtoUpdate.getId() );
+        user.name( userDtoUpdate.getName() );
+        user.username( userDtoUpdate.getUsername() );
+        user.password( userDtoUpdate.getPassword() );
+        user.phone( userDtoUpdate.getPhone() );
+        user.role( userDtoUpdate.getRole() );
+        user.turns( turnResponseDtoListToTurnList( userDtoUpdate.getTurns() ) );
+
+        return user.build();
+    }
+
+    @Override
+    public UserDto toUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto.UserDtoBuilder userDto = UserDto.builder();
+
+        userDto.id( user.getId() );
+        userDto.name( user.getName() );
+        userDto.username( user.getUsername() );
+        userDto.phone( user.getPhone() );
+        userDto.role( user.getRole() );
+        userDto.turns( turnListToTurnResponseDtoList( user.getTurns() ) );
+
+        return userDto.build();
+    }
+
+    @Override
+    public List<UserDto> toUserDtoList(List<User> users) {
+        if ( users == null ) {
+            return null;
+        }
+
+        List<UserDto> list = new ArrayList<UserDto>( users.size() );
+        for ( User user : users ) {
+            list.add( toUserDto( user ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public void updateUser(User user, UserDto userDto) {
+        if ( userDto == null ) {
+            return;
+        }
+
+        user.setId( userDto.getId() );
+        user.setName( userDto.getName() );
+        user.setUsername( userDto.getUsername() );
+        user.setPhone( userDto.getPhone() );
+        user.setRole( userDto.getRole() );
+        if ( user.getTurns() != null ) {
+            List<Turn> list = turnResponseDtoListToTurnList( userDto.getTurns() );
+            if ( list != null ) {
+                user.getTurns().clear();
+                user.getTurns().addAll( list );
+            }
+            else {
+                user.setTurns( null );
+            }
+        }
+        else {
+            List<Turn> list = turnResponseDtoListToTurnList( userDto.getTurns() );
+            if ( list != null ) {
+                user.setTurns( list );
+            }
+        }
+    }
+
+    @Override
+    public void updateUserUp(User user, UserDtoUpdate userDtoUpdate) {
+        if ( userDtoUpdate == null ) {
+            return;
+        }
+
+        user.setId( userDtoUpdate.getId() );
+        user.setName( userDtoUpdate.getName() );
+        user.setUsername( userDtoUpdate.getUsername() );
+        user.setPassword( userDtoUpdate.getPassword() );
+        user.setPhone( userDtoUpdate.getPhone() );
+        user.setRole( userDtoUpdate.getRole() );
+        if ( user.getTurns() != null ) {
+            List<Turn> list = turnResponseDtoListToTurnList( userDtoUpdate.getTurns() );
+            if ( list != null ) {
+                user.getTurns().clear();
+                user.getTurns().addAll( list );
+            }
+            else {
+                user.setTurns( null );
+            }
+        }
+        else {
+            List<Turn> list = turnResponseDtoListToTurnList( userDtoUpdate.getTurns() );
+            if ( list != null ) {
+                user.setTurns( list );
+            }
+        }
+    }
+
+    @Override
+    public UserDtoUpdate toUserDtoUpdate(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDtoUpdate.UserDtoUpdateBuilder userDtoUpdate = UserDtoUpdate.builder();
+
+        userDtoUpdate.id( user.getId() );
+        userDtoUpdate.name( user.getName() );
+        userDtoUpdate.username( user.getUsername() );
+        userDtoUpdate.password( user.getPassword() );
+        userDtoUpdate.phone( user.getPhone() );
+        userDtoUpdate.role( user.getRole() );
+        userDtoUpdate.turns( turnListToTurnResponseDtoList( user.getTurns() ) );
+
+        return userDtoUpdate.build();
+    }
+
+    @Override
+    public UserResponseDto toUserResponseDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponseDto.UserResponseDtoBuilder userResponseDto = UserResponseDto.builder();
+
+        userResponseDto.id( user.getId() );
+        userResponseDto.name( user.getName() );
+        userResponseDto.username( user.getUsername() );
+        userResponseDto.phone( user.getPhone() );
+
+        return userResponseDto.build();
+    }
+
+    @Override
+    public UserDtoUpdate toUserDtoUpdateByResponse(UserResponseDto userResponseDto) {
+        if ( userResponseDto == null ) {
+            return null;
+        }
+
+        UserDtoUpdate.UserDtoUpdateBuilder userDtoUpdate = UserDtoUpdate.builder();
+
+        userDtoUpdate.id( userResponseDto.getId() );
+        userDtoUpdate.name( userResponseDto.getName() );
+        userDtoUpdate.username( userResponseDto.getUsername() );
+        userDtoUpdate.phone( userResponseDto.getPhone() );
+
+        return userDtoUpdate.build();
+    }
+
+    @Override
+    public UserUpResponseDto toUserUpResponseDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserUpResponseDto.UserUpResponseDtoBuilder userUpResponseDto = UserUpResponseDto.builder();
+
+        userUpResponseDto.id( user.getId() );
+        userUpResponseDto.name( user.getName() );
+        userUpResponseDto.username( user.getUsername() );
+        userUpResponseDto.phone( user.getPhone() );
+        userUpResponseDto.role( user.getRole() );
+        userUpResponseDto.turns( turnListToTurnResponseDtoList( user.getTurns() ) );
+
+        return userUpResponseDto.build();
+    }
+
+    protected Turn turnResponseDtoToTurn(TurnResponseDto turnResponseDto) {
+        if ( turnResponseDto == null ) {
+            return null;
+        }
+
+        Turn.TurnBuilder turn = Turn.builder();
+
+        if ( turnResponseDto.getUser() != null ) {
+            if ( turn.getUser() == null ) {
+                turn.user( User.builder().build() );
+            }
+            updateUserUp( turn.getUser(), toUserDtoUpdateByResponse( turnResponseDto.getUser() ) );
+        }
+        turn.id( turnResponseDto.getId() );
+        turn.date( turnResponseDto.getDate() );
+        turn.hour( turnResponseDto.getHour() );
+        turn.status( turnResponseDto.getStatus() );
+
+        return turn.build();
+    }
+
+    protected List<Turn> turnResponseDtoListToTurnList(List<TurnResponseDto> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<Turn> list1 = new ArrayList<Turn>( list.size() );
+        for ( TurnResponseDto turnResponseDto : list ) {
+            list1.add( turnResponseDtoToTurn( turnResponseDto ) );
+        }
+
+        return list1;
+    }
+
+    protected TurnResponseDto turnToTurnResponseDto(Turn turn) {
+        if ( turn == null ) {
+            return null;
+        }
+
+        TurnResponseDto.TurnResponseDtoBuilder turnResponseDto = TurnResponseDto.builder();
+
+        turnResponseDto.id( turn.getId() );
+        turnResponseDto.date( turn.getDate() );
+        turnResponseDto.hour( turn.getHour() );
+        turnResponseDto.status( turn.getStatus() );
+        turnResponseDto.user( toUserResponseDto( turn.getUser() ) );
+
+        return turnResponseDto.build();
+    }
+
+    protected List<TurnResponseDto> turnListToTurnResponseDtoList(List<Turn> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<TurnResponseDto> list1 = new ArrayList<TurnResponseDto>( list.size() );
+        for ( Turn turn : list ) {
+            list1.add( turnToTurnResponseDto( turn ) );
+        }
+
+        return list1;
+    }
+}
