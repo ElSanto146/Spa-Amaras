e389cdc2387304ccf754ead9657372a7d3747d65
ElSanto146 <elsanto269@gmail.com>
2025-03-22 13:23:05 -0300

    Actualizado1

--- target/generated-sources/annotations/com/amaras/spa/mapper/ITurnMapperImpl.java
+++ target/generated-sources/annotations/com/amaras/spa/mapper/ITurnMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-08T14:19:14-0300",
+    date = "2025-03-22T13:21:53-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/com/amaras/spa/mapper/IUserMapperImpl.java
+++ target/generated-sources/annotations/com/amaras/spa/mapper/IUserMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-08T14:19:10-0300",
+    date = "2025-03-22T13:21:52-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.5 (Oracle Corporation)"
 )
 @Component
